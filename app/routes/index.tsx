c4757b6146d664dc7a9a7fc2247df7575f57d06f
Muhammad Jamiil Angnoo <jamiil.angnoo@yoyowallet.com>
2022-03-17 15:57:10 +0400

    gmd

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,21 +1,29 @@
 import { Box, Center, Container, Input } from "@chakra-ui/react";
-import { Form, json, useLoaderData } from "remix";
+import axios from "axios";
+import { Form, json, useLoaderData, LoaderFunction } from "remix";
+import { components } from "~/types/kyc";
 
-export async function loader({ request }) {
+export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
   const query = url.searchParams.get("search");
-  return json({ message: `Hello, ${query}` });
+
+  const baseUrl = process.env.GMD_URL;
+  const reqUrl = baseUrl + '/companies?q=' + query;
+
+  const response = await axios.get<components['schemas']['ListCompaniesResponse']>(reqUrl, {headers: {'x-api-key': process.env.GMD_API_KEY || ''}})
+
+  return response.data;
 }
 
 export default function Index() {
-  const data = useLoaderData();
+  const data = useLoaderData<components['schemas']['ListCompaniesResponse']>();
 
   return (
     <Center as={Container} h="100vh">
       <Box as={Form} method="get" minW="30ch">
         <Input type="text" placeholder="input text!" name="search" />
       </Box>
-      <Box>{data?.message}</Box>
+      <Box>{JSON.stringify(data)}</Box>
     </Center>
   );
 }
